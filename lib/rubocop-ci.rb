4b0b39197dde0be3ee6caffa6e9fac21f522e5d5
Niko Dziemba <niko@dziemba.com>
2015-03-17 18:08:25 +0100

    Simplify rake task loading

--- lib/rubocop-ci.rb
+++ lib/rubocop-ci.rb
@@ -1,11 +1,1 @@
-module RubocopCi
-  if defined?(Rails)
-    class Railtie < Rails::Railtie
-      railtie_name :rubocop_ci
-
-      rake_tasks do
-        load 'tasks/rubocop-ci.rake'
-      end
-    end
-  end
-end
+load 'tasks/rubocop-ci.rake'
